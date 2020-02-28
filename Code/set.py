fe5e8142ce9b433dc05eedabc799f13439a27b8f
Nyapal <nyapal14@gmail.com>
2020-02-27 22:05:12 -0800

    helpers

---
+++ Code/set.py
@@ -0,0 +1,40 @@
+# Choose a DS to implement your set with 
+#from binary import Binary
+#from hashtable import Hashtable 
+
+class Set:
+  '''initialize a new empty set structure, and add each element if a sequence is given'''
+  def __init__(self, elements=None):
+    pass 
+
+  '''size - property that tracks the number of elements in constant time'''
+  def size(self, idk):
+    pass
+  
+  '''contains(element) - return a boolean indicating whether element is in this set'''
+  def contains(self, element):
+    pass
+
+  '''add(element) - add element to this set, if not present already'''
+  def add(self, element):
+    pass
+
+  '''remove(element) - remove element from this set, if present, or else raise KeyError'''
+  def remove(self, element):
+    pass
+
+  '''union(other_set) - return a new set that is the union of this set and other_set'''
+  def union(self, other_set):
+    pass
+
+  '''intersection(other_set) - return a new set that is the intersection of this set and other_set'''
+  def intersection(self, other_set):
+    pass
+
+  '''difference(other_set) - return a new set that is the difference of this set and other_set'''
+  def difference(self, other_set):
+    pass
+
+  '''is_subset(other_set) - return a boolean indicating whether other_set is a subset of this set'''
+  def is_subset(self, other_set):
+    pass
